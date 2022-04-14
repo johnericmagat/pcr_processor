57343843a80b7ee04c39c4916aa6c66a01e9685a
John Eric Magat <johnericmagat@gmail.com>
2022-04-14 12:23:54 +0800

    Added models update. (20220414)

---
+++ pcr_processor/Model/UsersModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace pcr_processor.Model
+{
+	public class UsersModel
+	{
+		public Int32 Id { get; set; }
+		public String Name { get; set; }
+		public String Email { get; set; }
+		public DateTime Email_verified_at { get; set; }
+		public String Password { get; set; }
+		public String Remember_token { get; set; }
+		public DateTime Created_at { get; set; }
+		public DateTime Updated_at { get; set; }
+		public String User_type { get; set; }
+		public String Username { get; set; }
+	}
+}
