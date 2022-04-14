582a3982a6fec2b0bad42dc67009845e3f61e102
John Eric Magat <johnericmagat@gmail.com>
2022-04-14 15:04:10 +0800

    UI update. (20220414)

--- pcr_processor/MainWindow.xaml.cs
+++ pcr_processor/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using pcr_processor.Helper;
 using System;
 using System.Data;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace pcr_processor
 {
@@ -18,15 +20,47 @@ namespace pcr_processor
 
 		private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
 		{
-			FilterUsers();
 		}
 
-		private void FilterUsers()
+		private void BtnProcess_Click(object sender, RoutedEventArgs e)
+		{
+			Process();
+		}
+
+		private void Process()
 		{
 			try
 			{
 				DataTable users = new DataTable();
 				users = UsersBAL.FilterUsers();
+
+				double cnt = 0;
+				double inserted = 0;
+				bool hasError = false;
+				int errorCount = 0;
+
+				foreach (DataRow row in users.Rows)
+				{
+					cnt = users.Rows.Count;
+
+					inserted++;
+
+					double val = inserted / (cnt - errorCount);
+					double progress = val * 100;
+
+					ProgProcess.Dispatcher.Invoke(() => ProgProcess.Value = progress,
+						DispatcherPriority.Background);
+				}
+
+				string content = " record(s) has been successfully updated.";
+
+				if (hasError)
+				{
+					content = " record(s) has been successfully updated.\nThere is/are {0} record(s) not updated.";
+					content = String.Format(content, errorCount);
+				}
+				MessageBox.Show((cnt - errorCount).ToString(CultureInfo.InvariantCulture) + content, "PROCESS",
+					MessageBoxButton.OK, MessageBoxImage.Information);
 			}
 			catch (Exception ex)
 			{
